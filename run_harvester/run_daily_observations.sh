28149b4af8afb15a2ecc8290708c84f82a89097e
Jeffrey Tilson <jtilson@renci.org>
2022-05-26 19:26:24 -0400

    Re-enabled fetching NOAA data

--- run_harvester/run_daily_observations.sh
+++ run_harvester/run_daily_observations.sh
@@ -33,16 +33,16 @@ cd /projects/sequence_analysis/vol1/prediction_work/EDSASTAPPS/run_harvester
     for MONTHS in 05; do
     #for DAYS in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 31; do
     #for DAYS in 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 31; do
-    for DAYS in 18 19 20 21 22 23; do
+    for DAYS in 24 25 26; do
         # River data
-        $PHOME/python run_get_observation.py --stoptime '2022-'$MONTHS'-'$DAYS' 00:00:00'  --data_source 'CONTRAILS' --data_product 'river_water_level' --config_name "$CONT_AUTH/contrails.yml" --station_list "$STATIONDIR/contrails_stations_rivers.csv"
+        #$PHOME/python run_get_observation.py --stoptime '2022-'$MONTHS'-'$DAYS' 00:00:00'  --data_source 'CONTRAILS' --data_product 'river_water_level' --config_name "$CONT_AUTH/contrails.yml" --station_list "$STATIONDIR/contrails_stations_rivers.csv"
         # Contrails coastal data
-        $PHOME/python run_get_observation.py --stoptime '2022-'$MONTHS'-'$DAYS' 00:00:00' --data_source 'CONTRAILS' --data_product 'coastal_water_level' --config_name "$CONT_AUTH/contrails.yml" --station_list "$STATIONDIR/contrails_stations_coastal.csv"
+        #$PHOME/python run_get_observation.py --stoptime '2022-'$MONTHS'-'$DAYS' 00:00:00' --data_source 'CONTRAILS' --data_product 'coastal_water_level' --config_name "$CONT_AUTH/contrails.yml" --station_list "$STATIONDIR/contrails_stations_coastal.csv"
         # NOAA coastal data
         $PHOME/python run_get_observation.py --stoptime '2022-'$MONTHS'-'$DAYS' 00:00:00'  --data_source 'NOAA' --data_product "water_level" --station_list "$STATIONDIR/noaa_stations.csv"
         # NOAA predictions (tidal)
         $PHOME/python run_get_observation.py --stoptime '2022-'$MONTHS'-'$DAYS' 00:00:00'  --data_source 'NOAA' --data_product "predictions" --station_list "$STATIONDIR/noaa_stations.csv"
         ## NDBC BUOY DATA
-        $PHOME/python run_get_observation.py --stoptime '2022-'$MONTHS'-'$DAYS' 00:00:00'  --data_source 'NDBC' --data_product "wave_height" --station_list "$STATIONDIR/ndbc_buoys.csv"
+        #$PHOME/python run_get_observation.py --stoptime '2022-'$MONTHS'-'$DAYS' 00:00:00'  --data_source 'NDBC' --data_product "wave_height" --station_list "$STATIONDIR/ndbc_buoys.csv"
     done
 done
