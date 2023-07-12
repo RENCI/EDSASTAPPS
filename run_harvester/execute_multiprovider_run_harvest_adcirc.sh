73d9160eaff3f73ae74fad1eeba4f6971ef2c181
Jeffrey Tilson <jtilson@renci.org>
2023-07-12 10:49:58 -0400

    First attempt at multi provider support. Defaults to using fort61 lookups

---
+++ run_harvester/execute_multiprovider_run_harvest_adcirc.sh
@@ -0,0 +1,45 @@
+#/bin/bash -l
+#
+#  launcher sdcirc harvester script. This script is intended to be used by our k8s apsviz2 pipeline
+#
+# The input arguments have been expanded in this new launcher. Setting finalDir, will override any value of $RUNTIMEDIR
+# The log file will be copied to finalDir at the end of the processing
+#
+# The git repo that downloads station and grid data must be handled here
+#
+# Check environment
+
+URL=$1
+
+if [ -z "${PYTHONPATH:-}" ]; then
+   echo "PYTHONPATH is not set"
+   exit 1
+fi
+
+FINALDIR=$2
+
+if [ -z "${LOG_PATH:-}" ]; then
+   echo "LOG_PATH is not set: logs will be sent to $FINALDIR"
+   LOG_PATH="$FINALDIR"
+fi
+
+PROVIDER=$3
+
+# git clone the grid data. The underlying directory structure is implied within the provided grid_to_stationfile_maps.yml file
+git clone https://github.com/RENCI/AST_gridstations.git
+
+
+# Run the actual input URL first without changing the ensemble name
+
+echo "Begin ADCIRC Forecast fetch"
+python run_fetch_pipeline_adcirc_data_multiprovider_url_template.py --data_source 'TDS'  --url "$URL" --map_file  './AST_gridstations/full_stationlist/grid_to_stationfile_maps.yml'  --source_file './AST_gridstations/harvester_stations/sources_map.yaml' --data_provider "$PROVIDER" --finalDIR "$FINALDIR" --finalLOG "$LOG_PATH" 
+echo "Finished Forecast ADCIRC fetch"
+
+# Now we want to rerun the associated nowcast for thie input URL
+
+echo "Begin ADCIRC Nowcast fetch"
+python run_fetch_pipeline_adcirc_data_multiprovider_url_template.py --data_source 'TDS'  --url "$URL" --map_file  './AST_gridstations/full_stationlist/grid_to_stationfile_maps.yml'  --source_file './AST_gridstations/harvester_stations/sources_map.yaml' --data_provider "$PROVIDER" --finalDIR "$FINALDIR" --finalLOG "$LOG_PATH" --ensemble "nowcast"
+echo "Finish ADCIRC Nowcast fetch"
+
+echo "Finished FETCH ADCIRC $URL with status $?"
+
