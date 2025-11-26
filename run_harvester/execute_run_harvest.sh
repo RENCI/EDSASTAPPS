ac44bbeb996a33ec54bca6d7b327bf634dd91c8b
Jeffrey Tilson <jtilson@renci.org>
2025-11-26 08:53:22 -0500

    Removed noaa_datum arguments. Now rely on the station config yaml to specify them for calling NOAA

--- run_harvester/execute_run_harvest.sh
+++ run_harvester/execute_run_harvest.sh
@@ -60,6 +60,6 @@ sed -i 's/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/'"$CONTRAILS_KEY"'/g' ./secrets/c
 # We only need to supply the proper URL to start the job
 #
 
-python ./run_fetch_pipeline_observation.py --stoptime "$stoptime"  --finalLOG "$LOG_PATH" --finalDIR "$FINALDIR"  --map_source_file './AST_gridstations/harvester_stations/sources_map.yaml' --contrails_auth './secrets/contrails.yml' $NDAYSSET --noaa_datum "NAVD"
+python ./run_fetch_pipeline_observation.py --stoptime "$stoptime"  --finalLOG "$LOG_PATH" --finalDIR "$FINALDIR"  --map_source_file './AST_gridstations/harvester_stations/sources_map.yaml' --contrails_auth './secrets/contrails.yml' $NDAYSSET 
 
 echo "Finished RUN_HARVESTER with status $?"
